f94d9affafd61f2bba298cb36ad3812889e6518d
Danila Gorelko <danila@danilax86.space>
2022-10-23 23:12:23 +0300

    done with task 1

--- src/fp_lab_1/tasks/task_1.clj
+++ src/fp_lab_1/tasks/task_1.clj
@@ -1,14 +1,21 @@
-(ns fp-lab-1.tasks.task_1)
+(ns fp-lab-1.tasks.task-1)
+
+; Модульная реализация
+(def gen-seq
+  (range 1000))
+
+(def filtered-seq
+  (filter #(or
+             (zero? (rem % 3))
+             (zero? (rem % 5))) gen-seq))
+
+(def sum-seq-elms (reduce + filtered-seq))
 
 (defn modular []
-  (reduce + (filter #(or
-                       (zero? (rem % 3))
-                       (zero? (rem % 5)))
-                    (range 1000)))
+  sum-seq-elms
   )
 
-(modular)
-
+; Рекурсия
 (defn rec [n]
   (if (and (< n 1000))
     (if (or (zero? (rem n 3))
@@ -18,4 +25,33 @@
     0)
   )
 
-(rec 0)
+;; Хвостовая рекурсия
+(defn tail-rec [n]
+  (if (= n 0) 0
+              (if (or (zero? (rem n 3))
+                      (zero? (rem n 5)))
+                (+ n (tail-rec (- n 1)))
+                (tail-rec (- n 1)))
+              ))
+
+; Генерация с map
+(defn solve-map []
+  (reduce + (distinct                                       ; Убираем дубли
+              (into                                         ; Совмещаем две последовательности
+                (map #(* % 3) (range 1 334))
+                (map #(* % 5) (range 1 200)))))
+  )
+
+; Ленивые коллекции
+(defn positive-numbers
+  ([] (positive-numbers 1))
+  ([n] (lazy-seq
+         (cons n                                            ; Добавляем к последовательности элемент n
+               (positive-numbers (inc n))))))
+
+(defn solve-lazy []
+  (reduce + (filter #(or
+                       (zero? (rem % 3))
+                       (zero? (rem % 5)))
+                    (take 999 (positive-numbers))))
+  )
--- test/fp_lab_1/tasks/task_1_test.clj
+++ test/fp_lab_1/tasks/task_1_test.clj
@@ -1,12 +1,25 @@
 (ns fp-lab-1.tasks.task-1-test
   (:require [clojure.test :refer :all]
-            [fp-lab-1.tasks.task_1 :refer :all]))
+            [fp-lab-1.tasks.task-1 :refer :all]))
 
+(def answer 233168)
 
 (deftest modular-test-task-1
   (testing "Test task 1 modular"
-    (is (= (modular) 233168))))
+    (is (= (modular) answer))))
 
-(deftest reccursive-test-task-1
+(deftest recursive-test-task-1
   (testing "Test task 1 recursive"
-    (is (= (rec 0) 233168))))
+    (is (= (rec 0) answer))))
+
+(deftest tail-recursive-test-task-1
+  (testing "Test task 1 tail recursion"
+    (is (= (tail-rec 999) answer))))
+
+(deftest map-test-task-1
+  (testing "Test task 1 with map"
+    (is (= (solve-map) answer))))
+
+(deftest lazy-test-task-1
+  (testing "Test task 1 lazy-seq"
+    (is (= (solve-lazy) answer))))
